21c3588969e7887cf89e1e20eadc3b43954ba180
ravinmp <ravinmptr@gmail.com>
2022-04-01 21:14:59 +0700

    Auth Completed

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ListPlayer from "./pages/ListPlayer";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/listplayer" element={<ListPlayer />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
